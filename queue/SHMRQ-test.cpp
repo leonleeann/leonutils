c4c65609c766e09e45b3dfa0b9d22ef274b6c248
Leon <leon.lee.ann@gmail.com>
2023-11-26 00:12:53 +0800

    0.0.1 SHMRQ队列改为头文件实现，因为反正是模板，imp构型没意义，还容易内联

--- queue/SHMRQ-test.cpp
+++ queue/SHMRQ-test.cpp
@@ -1,11 +1,12 @@
-// #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <iomanip>
 #include <iostream>
+#include <map>
 
-#include "SHMRQ.tpp"
+#include "SHMRQ.hpp"
 
 using std::string;
+using Ptr2Bool_t = std::map<void*, bool>;
 
 namespace leon_utl {
 
@@ -13,63 +14,68 @@ namespace leon_utl {
  * 是原地调用 new/delete. 所以这里自己做的一个简易 mock 类.
  */
 struct ElementMock {
+	/* 在析构函数内部对自有成员变量的修改代码,会被gcc优化掉(你都要销毁了,就没必要执行了)
+	 * 所以不能简单用自有成员变量来记录析构函数是否被调用,只好弄个map来记录 */
+	static Ptr2Bool_t Destroyed;
+	static Ptr2Bool_t MvCreated;
+	static Ptr2Bool_t CpCreated;
+	static Ptr2Bool_t MvAssignd;
+	static Ptr2Bool_t CpAssignd;
+
 	~ElementMock() {
+		Destroyed[this] = true;
 		std::cerr << "销毁:" << this << std::endl;
-		destroyed = true;
 	};
 
 	ElementMock( int64_t pl_ ): payload( pl_ ) {};
 
 	ElementMock() {
 		std::cerr << "缺省构造:" << this << std::endl;
-		df_creatd = true;
 	};
 
 	ElementMock( ElementMock&& o ) {
 		payload = o.payload;
+		MvCreated[this] = true;
 		std::cerr << "移动构造:" << this << std::endl;
-		mv_creatd = true;
 	};
 
 	ElementMock( const ElementMock& o ) {
 		payload = o.payload;
+		CpCreated[this] = true;
 		std::cerr << "复制构造:" << this << std::endl;
-		cp_creatd = true;
 	};
 
 	ElementMock& operator=( ElementMock&& o ) {
 		payload = o.payload;
+		MvAssignd[this] = true;
 		std::cerr << "移动赋值:" << this << std::endl;
-		mv_assgnd = true;
 		return *this;
 	};
 
 	ElementMock& operator=( const ElementMock& o ) {
 		payload = o.payload;
+		CpAssignd[this] = true;
 		std::cerr << "复制赋值:" << this << std::endl;
-		cp_assgnd = true;
 		return *this;
 	};
 
 	void clear() {
 		payload = 0;
-		destroyed = false;
-		df_creatd = false;
-		mv_creatd = false;
-		cp_creatd = false;
-		mv_assgnd = false;
-		cp_assgnd = false;
+		Destroyed[this] = false;
+		MvCreated[this] = false;
+		CpCreated[this] = false;
+		MvAssignd[this] = false;
+		CpAssignd[this] = false;
 	};
 
 	int64_t	payload {};
-	// 确实来调用了析构器
-	bool	destroyed {};
-	bool	df_creatd {};
-	bool	mv_creatd {};
-	bool	cp_creatd {};
-	bool	mv_assgnd {};
-	bool	cp_assgnd {};
 };
+
+Ptr2Bool_t ElementMock::Destroyed {};
+Ptr2Bool_t ElementMock::MvCreated {};
+Ptr2Bool_t ElementMock::CpCreated {};
+Ptr2Bool_t ElementMock::MvAssignd {};
+Ptr2Bool_t ElementMock::CpAssignd {};
 
 using TestSHMRQ_t = SHMRQ_t<ElementMock>;
 
@@ -77,53 +83,85 @@ constexpr int64_t TEST_CAPA { 4 };
 
 struct TestSHMRQ_WITH_ELEMENT_F : public testing::Test {
 	TestSHMRQ_t		testee { "TestWithElement", TEST_CAPA, true };
-	ElementMock&	elmnt0 = testee._imp->_base[0].data;
-	ElementMock&	elmnt1 = testee._imp->_base[1].data;
-	ElementMock&	elmnt2 = testee._imp->_base[2].data;
-	ElementMock&	elmnt3 = testee._imp->_base[3].data;
+	ElementMock*	elmnt0 = & testee._base[0].data;
+	ElementMock*	elmnt1 = & testee._base[1].data;
+	ElementMock*	elmnt2 = & testee._base[2].data;
+	ElementMock*	elmnt3 = & testee._base[3].data;
 
 	void SetUp() override {
 		ASSERT_EQ( testee.capa(), TEST_CAPA );
 		ASSERT_EQ( testee.size(), 0 );
-		elmnt0.clear();
-		elmnt1.clear();
-		elmnt2.clear();
-		elmnt3.clear();
+		elmnt0->clear();
+		elmnt1->clear();
+		elmnt2->clear();
+		elmnt3->clear();
+		std::cout << "elmnt0创建于:" << elmnt0 << std::endl;
+		std::cout << "elmnt1创建于:" << elmnt1 << std::endl;
+		std::cout << "elmnt2创建于:" << elmnt2 << std::endl;
+		std::cout << "elmnt3创建于:" << elmnt3 << std::endl;
 	};
 };
 
 TEST_F( TestSHMRQ_WITH_ELEMENT_F, elementLifeCycle ) {
-	ElementMock tmp { 111 };
+	ElementMock tmp { -1 };
 
 //==== 入队 ====================================================================
 	// 如果以复制方式入队的话, "复制构造器"应得到调用
-	ASSERT_FALSE( elmnt0.cp_creatd );
+	ASSERT_FALSE( ElementMock::CpCreated[elmnt0] );
 	ASSERT_TRUE( testee.enque( tmp ) );
-	ASSERT_TRUE( elmnt0.cp_creatd );
+	ASSERT_TRUE( ElementMock::CpCreated[elmnt0] );
 	ASSERT_EQ( testee.size(), 1 );
-	ASSERT_EQ( elmnt0.payload, tmp.payload );
+	ASSERT_EQ( elmnt0->payload, tmp.payload );
 
 	// 如果以移动方式入队的话, "移动构造器"应得到调用
-	ASSERT_FALSE( elmnt1.mv_creatd );
-	ASSERT_TRUE( testee.enque( ElementMock { 222 } ) );
-	ASSERT_TRUE( elmnt1.mv_creatd );
+	tmp.payload = 111;
+	ASSERT_FALSE( ElementMock::MvCreated[elmnt1] );
+	ASSERT_TRUE( testee.enque( std::move( tmp ) ) );
+	ASSERT_TRUE( ElementMock::MvCreated[elmnt1] );
 	ASSERT_EQ( testee.size(), 2 );
-	ASSERT_EQ( elmnt1.payload, 222 );
+	ASSERT_EQ( elmnt1->payload, 111 );
+
+	ASSERT_FALSE( ElementMock::MvCreated[elmnt2] );
+	ASSERT_TRUE( testee.enque( ElementMock { 222 } ) );
+	ASSERT_TRUE( ElementMock::MvCreated[elmnt2] );
+	ASSERT_EQ( testee.size(), 3 );
+	ASSERT_EQ( elmnt2->payload, 222 );
+
+	ASSERT_FALSE( ElementMock::MvCreated[elmnt3] );
+	ASSERT_TRUE( testee.enque( ElementMock { 333 } ) );
+	ASSERT_TRUE( ElementMock::MvCreated[elmnt3] );
+	ASSERT_EQ( testee.size(), 4 );
+	ASSERT_EQ( elmnt3->payload, 333 );
+
+	// 队列已满,再入队应该失败
+	ASSERT_FALSE( testee.enque( ElementMock { 444 } ) );
+	ASSERT_EQ( testee.size(), 4 );
 
 //==== 出队 ====================================================================
-	tmp.payload = -1;
-	ASSERT_FALSE( elmnt0.destroyed );
+	tmp.payload = -2;
+	ASSERT_FALSE( ElementMock::Destroyed[elmnt0] );
 	ASSERT_TRUE( testee.deque( tmp ) );
-	ASSERT_TRUE( elmnt0.destroyed );
-	ASSERT_EQ( testee.size(), 1 );
+	ASSERT_TRUE( ElementMock::Destroyed[elmnt0] );
+	ASSERT_EQ( testee.size(), 3 );
+	ASSERT_EQ( tmp.payload, -1 );
+
+	ASSERT_FALSE( ElementMock::Destroyed[elmnt1] );
+	ASSERT_TRUE( testee.deque( tmp ) );
+	ASSERT_TRUE( ElementMock::Destroyed[elmnt1] );
+	ASSERT_EQ( testee.size(), 2 );
 	ASSERT_EQ( tmp.payload, 111 );
 
-	tmp.payload = -1;
-	ASSERT_FALSE( elmnt1.destroyed );
+	ASSERT_FALSE( ElementMock::Destroyed[elmnt2] );
 	ASSERT_TRUE( testee.deque( tmp ) );
-	ASSERT_TRUE( elmnt1.destroyed );
-	ASSERT_EQ( testee.size(), 0 );
+	ASSERT_TRUE( ElementMock::Destroyed[elmnt2] );
+	ASSERT_EQ( testee.size(), 1 );
 	ASSERT_EQ( tmp.payload, 222 );
+
+	ASSERT_FALSE( ElementMock::Destroyed[elmnt3] );
+	ASSERT_TRUE( testee.deque( tmp ) );
+	ASSERT_TRUE( ElementMock::Destroyed[elmnt3] );
+	ASSERT_EQ( testee.size(), 0 );
+	ASSERT_EQ( tmp.payload, 333 );
 };
 
 };	// namespace leon_utl
--- queue/SHMRQ.hpp
+++ queue/SHMRQ.hpp
@@ -1,22 +1,86 @@
 #pragma once
+#include <algorithm>	// erase, fill, for_each, max, min, remove, sort, swap
+#include <atomic>
+#include <bit>
+#include <cstring>		// strlen, strncmp, strncpy, memset, memcpy, memmove, strerror
+#include <fcntl.h>		// O_RDONLY, S_IRUSR, S_IWUSR
+#include <filesystem>
+#include <iostream>
 #include <string>
+#include <sys/mman.h>	// shm_open, PROT_READ, PROT_WRITE, MAP_PRIVATE, MAP_ANON
 
 /* 一个基于共享内存可跨进程使用的无锁环状队列
  * 特点:
  *   1.无锁
  *   2.环状队列,整个生命周期不会追加/释放内存
  *   3.多生产者/多消费者(MPMC)
- *   4.不论多少个生产者、消费者,本对象的拥有者(owner)只能有一个
- *   5.基于共享内存,可多进程使用,也可多线程使用
- *   6.曾经使用无符号的 size_t 作为容量类型是 STL 的历史错误,所以改用 int64_t
+ *   4.基于共享内存,可多进程使用,也可多线程使用
+ *
+ * 注意!!!!!!!!!!!!!!!!!!!
+ *   1.不论多少个生产者、消费者,本对象的拥有者(owner)只能有一个,它负责创建共享内存
+ *   2.跨进程使用时,队列的载荷不能是指针!
+ *   3.曾经使用无符号的 size_t 作为容量类型是 STL 的历史错误,所以改用 int64_t
  */
+
 namespace leon_utl {
+
+inline constexpr std::memory_order mo_acq_rel = std::memory_order::acq_rel;
+inline constexpr std::memory_order mo_acquire = std::memory_order::acquire;
+inline constexpr std::memory_order mo_consume = std::memory_order::consume;
+inline constexpr std::memory_order mo_relaxed = std::memory_order::relaxed;
+inline constexpr std::memory_order mo_release = std::memory_order::release;
+inline constexpr std::memory_order mo_seq_cst = std::memory_order::seq_cst;
+
+static std::string GetValidSHMName( const std::string& org_ ) {
+	// 名字的全局唯一性由使用者解决
+	std::string qn = std::filesystem::path( org_.c_str() ).stem().c_str();
+	qn.erase( std::remove( qn.begin(), qn.end(), ' ' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '!' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '"' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '#' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '$' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '%' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '&' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '(' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), ')' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '*' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '+' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), ',' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '-' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '.' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '/' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), ':' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), ';' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '<' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '=' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '>' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '?' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '@' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '[' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '\'' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '\\' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), ']' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '^' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '`' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '{' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '|' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '}' ), qn.end() );
+	qn.erase( std::remove( qn.begin(), qn.end(), '~' ), qn.end() );
+	return '/' + qn;
+};
 
 template <typename T>
 class SHMRQ_t {
 	friend struct TestSHMRQ_WITH_ELEMENT_F;
 
 public:
+	struct Node_t {
+		alignas( 64 )
+		std::atomic_int64_t	head;
+		std::atomic_int64_t	tail;
+		T					data;
+	};
+
 	// 最大内存占用(单位:字节)
 	static constexpr int64_t MAX_MEM_USAGE = 0x40000000;  // 1G Bytes
 
@@ -27,18 +91,17 @@ public:
 	explicit SHMRQ_t( const std::string& shm_name, int64_t capacity, bool owning );
 	~SHMRQ_t();
 
-	const std::string& name() const;
+	// 向OS申请的共享内存名称
+	const std::string& name() const { return _shmn; };
 
 	// 队列容量(单位:元素个数)
-	int64_t capa() const;
+	int64_t capa() const { return _capa; };
 
 	// 当前占用(单位:元素个数)
-	int64_t size() const;
+	int64_t size() const {
+		return _tail.load( mo_relaxed ) - _head.load( mo_acquire );
+	};
 
-	// 复制入队
-	// bool enque( const T& data );
-	// 移动入队
-	// bool enque( T&& data );
 	// 通用入队(不再区分复制还是移动)
 	template<typename U>
 	bool enque( U&& data );
@@ -47,8 +110,136 @@ public:
 	bool deque( T& data );
 
 protected:
-	struct Imp_t;
-	Imp_t* _imp;
+	// 对齐到64字节(一个CPU cache line的大小),保证以下变量在同一个cache line里面
+	alignas( 64 )
+	// 基址
+	Node_t* const		_base = nullptr;
+	// 容量
+	const int64_t		_capa = 0;
+	// 访问掩码
+	const int64_t		_mask = 0;
+	// raw buffer
+	void*				_rawb = nullptr;
+	// 共享内存名称
+	const std::string	_shmn;
+	// 我是共享内存的创建者/拥有者(owner)
+	bool				_ownr;
+
+	// 对齐到64字节(一个CPU cache line的大小),保证_tail,_head不在同一个line里面
+	alignas( 64 )
+	std::atomic_int64_t	_head = {};
+	alignas( 64 )
+	std::atomic_int64_t	_tail = {};
+};
+
+template <typename T>
+inline SHMRQ_t<T>::SHMRQ_t( const std::string& shm_, int64_t capa_, bool own_ )
+	: _shmn( GetValidSHMName( shm_ ) ), _ownr( own_ ) {
+	/*======== 确定容量 =============================================================
+			1.最大内存占用不能超过 MAX_MEM_USAGE 字节
+			2.让容量刚好是2的整数次幂,为了保证 _mask 低位必须是全1
+			3.容量至少保证 LEAST_ELEMNTS, 避免头尾重叠
+			等价算法:
+			int64_t MOST_ELEMENTS = std::log2( MAX_MEM_USAGE / sizeof( Node_t ) );
+			MOST_ELEMENTS = std::pow( 2, MOST_ELEMENTS );
+			capa_ = std::pow( 2, std::ceil( std::log2( capa_ ) ) );
+			*/
+
+#if( __GNUC__ >= 10 )
+	// gcc-10 及之后要用这个
+	constexpr int64_t MOST_ELEMENTS = std::bit_floor( MAX_MEM_USAGE / sizeof( Node_t ) );
+	capa_ = std::bit_ceil( static_cast<size_t>( capa_ ) );
+#else
+	// gcc-9 及之前要用这个
+	constexpr int64_t MOST_ELEMENTS = std::floor2( MAX_MEM_USAGE / sizeof( Node_t ) );
+	capa_ = std::ceil2( capa_ );
+#endif
+
+	capa_ = std::min( capa_, MOST_ELEMENTS );
+	capa_ = std::max( capa_, LEAST_ELEMNTS );
+	const_cast<int64_t&>( _capa ) = capa_;
+	const_cast<int64_t&>( _mask ) = _capa - 1;
+	int64_t bytes = _capa * sizeof( Node_t );
+
+//======== 分配内存 =============================================================
+	int shm_fd;
+	if( _ownr )
+		shm_fd = shm_open( _shmn.c_str(), O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR );
+	else
+		shm_fd = shm_open( _shmn.c_str(), O_RDWR, S_IRUSR | S_IWUSR );
+	if( shm_fd < 0 )
+		throw std::runtime_error( _shmn + ":shm_open error:" + strerror( errno ) );
+	if( ftruncate( shm_fd, bytes ) != 0 )
+		throw std::runtime_error( _shmn + ":ftruncate error:" + strerror( errno ) );
+	_rawb = mmap( NULL, bytes, PROT_READ | PROT_WRITE, MAP_SHARED_VALIDATE, shm_fd, 0 );
+	if( _rawb == MAP_FAILED )
+		throw std::runtime_error( _shmn + ":mmap error:" + strerror( errno ) );
+	if( close( shm_fd ) != 0 )
+		throw std::runtime_error( _shmn + ":close(shm_fd) error:" + strerror( errno ) );
+
+	// 对齐到整64字节边界
+	if( ( reinterpret_cast<uintptr_t>( _rawb ) & 63 ) != 0 )
+		throw std::runtime_error( _shmn + ":地址未从64字节整倍数开始!" );
+
+//======== 初始化 ===============================================================
+	const_cast<Node_t*&>( _base ) = reinterpret_cast<Node_t*>( _rawb );
+	for( int64_t i = 0; i < _capa; ++i ) {
+		auto& node = _base[i];
+		node.head.store( -1, mo_relaxed );
+		node.tail.store( i, mo_relaxed );
+		// 从OS申请的共享内存都是全零，可以不必初始化 data
+	}
+	_head.store( 0, mo_relaxed );
+	_tail.store( 0, mo_relaxed );
+};
+
+template <typename T>
+inline SHMRQ_t<T>::~SHMRQ_t() {
+	// 如果队内还剩有效数据,逐一调用它们的析构器,确保它们内部的资源释放(比如uniq_ptr)
+	int64_t tail = _tail.load();
+	for( int64_t j = _head.load(); j != tail; ++j )
+		_base[ j & _mask ].data.~T();
+
+	// 析构器里面不能抛异常
+	if( munmap( _rawb, _capa * sizeof( Node_t ) ) != 0 )
+		std::cerr << _shmn << ":munmap:" << strerror( errno ) << std::endl;
+	if( _ownr && shm_unlink( _shmn.c_str() ) != 0 )
+		std::cerr << _shmn << ":shm_unlink:" << strerror( errno ) << std::endl;
+};
+
+template <typename T>
+template <typename U>
+inline bool SHMRQ_t<T>::enque( U&& src_ ) {
+	int64_t tail = _tail.load( mo_relaxed );
+	Node_t* node;
+	do {
+		node = _base + ( tail & _mask );
+		if( node->tail.load( mo_relaxed ) != tail )
+			// 队列已满
+			return false;
+	} while( ! _tail.compare_exchange_weak( tail, tail + 1, mo_relaxed ) );
+
+	// new( & node->data ) T( std::move( src_ ) );
+	new( & node->data ) T( std::forward<U>( src_ ) );
+	node->head.store( tail, mo_release );
+	return true;
+};
+
+template <typename T>
+bool SHMRQ_t<T>::deque( T& dest_ ) {
+	int64_t head = _head.load( mo_relaxed );
+	Node_t* node;
+	do {
+		node = _base + ( head & _mask );
+		if( node->head.load( mo_relaxed ) != head )
+			// 队列已空或者恰好该元素尚未就绪(碰撞)
+			return false;
+	} while( ! _head.compare_exchange_weak( head, head + 1, mo_relaxed ) );
+
+	dest_ = std::move( node->data );
+	node->data.~T();
+	node->tail.store( head + _capa, mo_release );
+	return true;
 };
 
 };  // namespace leon_utl
