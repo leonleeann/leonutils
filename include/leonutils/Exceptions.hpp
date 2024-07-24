10fe252a27e79329319b70bc11f9db588408ee05
Leon <leon.lee.ann@gmail.com>
2024-07-24 16:46:38 +0800

    0.1.2 增加及调整异常定义

--- include/leonutils/Exceptions.hpp
+++ include/leonutils/Exceptions.hpp
@@ -7,28 +7,32 @@ namespace leon_utl {
 using str_t = std::string;
 
 // 错误地使用
-class bad_usage : public std::logic_error {
+class bad_usage: public std::logic_error {
 public:
-	// using std::logic_error::logic_error;
-	explicit bad_usage( const str_t& err_msg ):
-		std::logic_error( err_msg ) {};
+	explicit bad_usage( const str_t& err_ ):
+		std::logic_error( err_ ) {};
 
-	explicit bad_usage( const char* err_msg ):
-		std::logic_error( err_msg ) {};
+	explicit bad_usage( const char* err_ ):
+		std::logic_error( err_ ) {};
 };
 
 // 下标越界
-class bad_index : public std::runtime_error {
+class bad_index: public std::out_of_range {
 public:
-	// using std::runtime_error::runtime_error;
+	explicit bad_index(): std::out_of_range( "下标越界" ) {};
 
-	explicit bad_index(): std::runtime_error( "下标越界" ) {};
+	explicit bad_index( const str_t& err_ ): std::out_of_range( err_ ) {};
 
-	explicit bad_index( const str_t& err_msg ):
-		std::runtime_error( err_msg ) {};
+	explicit bad_index( const char* err_ ): std::out_of_range( err_ ) {};
+};
 
-	explicit bad_index( const char* err_msg ):
-		std::runtime_error( err_msg ) {};
+class parse_err: public std::runtime_error {
+public:
+	explicit parse_err(): std::runtime_error( "解析失败" ) {};
+
+	explicit parse_err( const str_t& err_ ): std::runtime_error( err_ ) {};
+
+	explicit parse_err( const char* err_ ): std::runtime_error( err_ ) {};
 };
 
 }; //namespace leon_utl
