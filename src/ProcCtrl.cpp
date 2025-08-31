63e20d1cef3c857813f62a5ed60c0a76655e86c8
Leon <leon.lee.ann@gmail.com>
2025-08-31 10:15:34 +0800

    0.3.15 forkExec采用定长数组传递CLI参数避免超界

--- src/ProcCtrl.cpp
+++ src/ProcCtrl.cpp
@@ -27,23 +27,23 @@ pid_t ProcCtrlImp_t::forkOnly() {
 
 pid_t ProcCtrlImp_t::forkExec( const str_t& bin_, const CliArgs_t& args_ ) {
 	/* 进到本函数时,日志系统应该是关闭的. 为了让 主、 子 进程输出互不干扰,
-		本函数内不用 LeonLog. 而且, 子进程日志输出到 stderr
-	*/
+		本函数内不用 LeonLog. 而且, 子进程日志输出到 stderr */
+
+	/* 实际运行中发现: 在父进程中, 执行了本函数之后, 其它内存区被意外覆盖了.
+		为排除是本函数的问题, 在此采用定长数组保存 CLI 参数集, 确保数组一定不会超界. */
+	char* c_args[100];
+	if( ( args_.size() + 1 ) * 2 > 100 )
+		throw std::out_of_range( "ProcCtrl::forkExec内部当前采用定长数组[100]保存命令行参数, 已超限制!" );
 
 	pid_t child = fork();
-	if( child < 0 ) {
-		std::cerr << "fork失败.无法继续!" << std::endl;
-		exit( EXIT_FAILURE );
-	}
-
+	if( child < 0 )
+		throw std::out_of_range("ProcCtrl::fork失败.无法继续!");
 	if( child != 0 )
 		// 这是父进程
 		return child;
 
 	// 这是子进程
-	char* c_args[( args_.size() + 1 ) * 2];
 	int i { 0 };
-
 	// 第0个参数是 可执行文件名 本身
 	c_args[i++] = const_cast<char*>( bin_.c_str() );
 
@@ -56,14 +56,13 @@ pid_t ProcCtrlImp_t::forkExec( const str_t& bin_, const CliArgs_t& args_ ) {
 			c_args[i++] = const_cast<char*>( k.c_str() );
 			c_args[i++] = const_cast<char*>( v.c_str() );
 		}
-
 	// 末尾补一个空指针,表示数组结束
 	c_args[i] = nullptr;
 
 	// 执行进去了就不会返回来了
 	execv( bin_.c_str(), c_args );
 	// 如果回来,必然是错误!
-	std::cerr << "exec:" << bin_ << "出错!" << std::endl;
+	std::cerr << "ProcCtrl::exec:" << bin_ << "出错!" << std::endl;
 	exit( EXIT_FAILURE );
 };
 
