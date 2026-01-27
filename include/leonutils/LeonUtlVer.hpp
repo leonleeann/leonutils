f1dc4002b9455e1b418b945aa948273a271bcb13
Leon <leon.lee.ann@gmail.com>
2026-01-27 10:21:26 +0800

    0.3.25 可纳入其它项目统一编译

---
+++ include/leonutils/LeonUtlVer.hpp
@@ -0,0 +1,12 @@
+// 项目全局配置，各项值由CMake每次build时自动填入，请勿手动修改本文件！
+
+#pragma once
+
+#define PROJECT_VERSION_MAJOR (0)
+#define PROJECT_VERSION_MINOR (3)
+#define PROJECT_VERSION_PATCH (25)
+#define PROJECT_RELEASE_NOTES ("")
+#define PROJECT_VERSION       ("0.3.25")
+#define PROJECT_DESCRIPTION   ("leon's c++ utils")
+#define CMAKE_BUILD_TYPE ( "Release" )
+#define CMAKE_CXX_FLAGS ( "" )
